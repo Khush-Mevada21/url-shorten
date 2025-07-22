c7b3e2fa35c3a44481f94873801d80a5fa7d2519
Khush-Mevada21 <106475063+Khush-Mevada21@users.noreply.github.com>
2025-07-22 19:51:29 +0530

    custom alias

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -10,12 +10,25 @@ client.connect();
 const BASE_URL = process.env.BASE_URL;
 
 exports.shortenURL = async (req, res) => {
-    const { longUrl } = req.body;
+    const { longUrl, customAlias } = req.body;
 
     if (!longUrl) return res.status(400).json({ error: 'URL is required' });
 
-    const shortId = nanoid(6);
+    const shortId = customAlias || nanoid(6);
+
+    const exists = await client.get(shortId);
+    if (exists) {
+        return res.status(409).json({ error: 'Alias already are taken' });
+    }
+
+
     const shortUrl = `${BASE_URL}/${shortId}`;
+    function isValidAlias(alias) {
+        return /^[a-zA-Z0-9_-]{3,30}$/.test(alias);
+    }
+    if(!isValidAlias){
+        return res.status(400).json({error: 'Alias not valid !'})
+    }
 
     await client.set(shortId, longUrl, { EX: 3600}); // Expires in 1 hour
 
