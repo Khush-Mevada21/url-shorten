058a457950aaea0df4cbbc4cb9487a7b65b3adf1
Khush-Mevada21 <106475063+Khush-Mevada21@users.noreply.github.com>
2025-07-22 20:22:11 +0530

    custom expiresIn

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,6 +1,8 @@
 const redis = require('redis');
 const { nanoid } = require('nanoid');
 require('dotenv').config();
+const ms = require('ms');
+
 
 const client = redis.createClient({
     url: process.env.REDIS_URL,
@@ -10,7 +12,7 @@ client.connect();
 const BASE_URL = process.env.BASE_URL;
 
 exports.shortenURL = async (req, res) => {
-    const { longUrl, customAlias } = req.body;
+    const { longUrl, customAlias, expiresIn } = req.body;
 
     if (!longUrl) return res.status(400).json({ error: 'URL is required' });
 
@@ -22,16 +24,41 @@ exports.shortenURL = async (req, res) => {
     }
 
 
-    const shortUrl = `${BASE_URL}/${shortId}`;
+    
     function isValidAlias(alias) {
         return /^[a-zA-Z0-9_-]{3,30}$/.test(alias);
     }
-    if(!isValidAlias){
-        return res.status(400).json({error: 'Alias not valid !'})
+
+    if (customAlias && !isValidAlias(customAlias)) {
+        return res.status(400).json({ error: 'Alias not valid!' });
     }
 
-    await client.set(shortId, longUrl, { EX: 3600}); // Expires in 1 hour
 
+    let expireSeconds = 3600; // Set Expiry time in 10s, 10m, 1h
+
+    if (expiresIn){
+        try{
+            if(typeof expiresIn === 'string'){
+                expireSeconds = Math.floor(ms(expiresIn) / 1000);
+
+            }
+            else if(typeof expiresIn === 'number'){
+                expireSeconds = expiresIn
+            }
+        }
+        catch(err){
+            return res.status(400).json({error: "Invalid expireIn Format !"})
+        }
+    }
+
+    if (expireSeconds <= 0) {
+        return res.status(400).json({ error: 'Expiration time must be greater than 0' });
+    }
+    console.log("URL Expires In :", expireSeconds);
+
+    await client.set(shortId, longUrl, { EX: expireSeconds });
+
+    const shortUrl = `${BASE_URL}/${shortId}`;
     res.json({ shortUrl });
 };
 
